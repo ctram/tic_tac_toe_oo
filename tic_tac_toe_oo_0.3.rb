47b98f89c5ea62ff6a8aa57f26100b3e255f1080
Chris Tram <chris.tram@gmail.com>
2014-11-07 19:50:17 -0800

    More codegit add .!

--- tic_tac_toe_oo_0.3.rb
+++ tic_tac_toe_oo_0.3.rb
@@ -46,7 +46,7 @@ class Game
       puts "Pick a square to mark (1-9):"
       print ">>"
       users_choice = gets.chomp
-    end until self.board.squares[users_choice] = " "
+    end until self.board.squares[users_choice] == " "
 
     user == player_x ? user_sym = "X" : user_sym = "O"
 
@@ -56,19 +56,21 @@ class Game
   def find_winner
     s = self.board.squares
 
-    if  # Horizontal wins
+    if  (
+        # Horizontal wins
         (s[1] == s[2] and s[2] == s[3] and s[3] != " ") or
         (s[4] == s[5] and s[5] == s[6] and s[6] != " ") or
         (s[7] == s[8] and s[8] == s[9] and s[9] != " ") or
 
         # Vertical wins
-        (s[1] == s[4] and s)[4] ==s[7] and s[7] != " ") or
+        (s[1] == s[4] and s[4] ==s[7] and s[7] != " ") or
         (s[2] == s[5] and s[5] == s[8] and s[8] != " ") or
         (s[3] == s[6] and s[6] == s[9] and s[9] != " ") or
 
         # Diagonal wins
         (s[1] == s[5] and s[5] == s[9] and s[9] != " ") or
         (s[3] == s[5] and s[5] == s[7] and s[7] != " ")
+                                                                )
 
       # There is a winner, deteminer who it is.
       # If X won, then there will be more X's on the board, since X goes first and will always be ahead one move on his win.
@@ -76,21 +78,26 @@ class Game
       num_os = 0
 
       self.board.squares.each do |square|
-        square == "X" ? num_xs += 1
-        square == "O" ? num_os += 1
+        square == "X" ? num_xs += 1 : nil
+        square == "O" ? num_os += 1 : nil
       end
 
-      return num_xs > num_os ?  player_x :  player_o
+      num_xs > num_os ? (return  player_x) : (return  player_o)
 
     end
 
   end
 
-  # TODO: code is_board_full?
   def is_board_full?
+    empty_squares = 0
+    self.board.squares.each do |square|
+      if square == " "
+        empty_squares += 1
+      end
+    end
+
+    empty_squares == 0 ? (return true ): (return false)
   end
-
-
 
   def run
     puts "Welcome to Tic Tac Toe!"
@@ -100,46 +107,61 @@ class Game
     user = Player.new(name)
     comp = Player.new("Comp")
 
-    acceptable_input = %w( x o )
-    input = nil
-    until acceptable_input.include? input
-      puts "Choose to play X or O (X gets to go first!):"
-      input = gets.chomp.downcase
-    end
-
-    # Set shapes players have chosen.
-    input == "x" ? (player_x = user; player_o = comp) : (player_x = comp; player_o = user)
-
-
     begin
-      # TODO: code meat of choosing squares
+      acceptable_input = %w( x o )
+      input = nil
+      until acceptable_input.include? input
+        puts "Choose to play X or O (X gets to go first!):"
+        input = gets.chomp.downcase
+      end
 
-      # player x goes
-      if user == player_x
-        # TODO: code player picks square method
-        user_picks_square()
-        find_winner() == nil ? nil : bool_has_winner = true
-        bool_has_winner == true ? (winner = find_winner(); break) : nil
-        is_board_full() == true ? (bool_board_full = true; break) : nil
-        # XXX:
+      # Set shapes players have chosen.
+      input == "x" ? (player_x = user; player_o = comp) : (player_x = comp; player_o = user)
 
+
+      begin
+          draw_board()
+        if user == player_x
+          user_picks_square()
+          draw_board()
+          find_winner() == nil ? nil : bool_has_winner = true
+          bool_has_winner == true ? (winner = find_winner(); break) : nil
+          is_board_full() == true ? (bool_board_full = true; break) : nil
+
+          comp_picks_square()
+          draw_board()
+          find_winner() == nil ? nil : bool_has_winner = true
+          bool_has_winner == true ? (winner = find_winner(); break) : nil
+          is_board_full() == true ? (bool_board_full = true; break) : nil
+
+        else
+          comp_picks_square()
+          draw_board()
+          find_winner() == nil ? nil : bool_has_winner = true
+          bool_has_winner == true ? (winner = find_winner(); break) : nil
+          is_board_full() == true ? (bool_board_full = true; break) : nil
+
+          user_picks_square()
+          draw_board()
+          find_winner() == nil ? nil : bool_has_winner = true
+          bool_has_winner == true ? (winner = find_winner(); break) : nil
+          is_board_full() == true ? (bool_board_full = true; break) : nil
+        end
+      end until bool_has_winner or bool_board_full
+
+      if bool_has_winner
+        winner == user ? winner_name = user.name : winner_name = comp.name
+        puts "#{winner_name} wins!"
       else
-        comp_picks_square()
+        puts "The board is full. It's a tie!"
+      end
 
+      puts "Press q to quit. Otherwise, press any key to play again!"
+      print ">>"
+      input = gets.chomp
+      input == "q" ? (bool_user_wants_quit = true) : nil
 
-
-
-      #
-      # player y goes
-      # check for winner
-      # check for full board
-      #
-    end until bool_has_winner or bool_board_full
-
-
-
-
-
+    end until bool_user_wants_quit == true
   end
 
 end
